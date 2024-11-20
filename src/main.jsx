08a4d63e59c62166cea18274e97fccc7d73a4d40
Shauna Mack <shaunakatemack@gmail.com>
2024-11-20 17:41:11 +1000

    define the routes that decides what cotent to render on the page

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
+// Here we import out pages
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+
+// Here we create our router and tell it whats pages to render at what path
+const router = createBrowserRouter([
+  // These are the three routes!
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
+  },
+  {
+    path: "/contact",
+    element: <ContactPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    {/* Here we wrap our app in the router provider so the pages render */}
+    <RouterProvider router={router} />
   </StrictMode>
 );
