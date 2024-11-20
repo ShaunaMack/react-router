c53916768076046fae0b5ea44b78bfe7b345d906
Shauna Mack <shaunakatemack@gmail.com>
2024-11-20 17:45:04 +1000

    call the nav bar component in main so we can click through to the different urls and have the relevant content load

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,25 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App.jsx";
 // Here we import out pages
 import HomePage from "./pages/HomePage.jsx";
 import AboutPage from "./pages/AboutPage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
+// Here we import our components
+import NavBar from "./components/NavBar.jsx";
 
 // Here we create our router and tell it whats pages to render at what path
 const router = createBrowserRouter([
   // These are the three routes!
   {
     path: "/",
-    element: <HomePage />,
-  },
-  {
-    path: "/about",
-    element: <AboutPage />,
-  },
-  {
-    path: "/contact",
-    element: <ContactPage />,
+    // Putting our NavBar as the main component will causes the children to render in the <Outlet />
+    element: <NavBar />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/about", element: <AboutPage /> },
+      { path: "/contact", element: <ContactPage /> },
+    ],
   },
 ]);
 
